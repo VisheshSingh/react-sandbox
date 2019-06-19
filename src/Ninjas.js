b9e446a6f850368b57767f948ffcf741fd35605b
v-boy <visheshsingh16@gmail.com>
2019-06-18 20:19:44 -0500

    Nesting components

---
+++ src/Ninjas.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Ninjas extends Component {
+  render() {
+    return (
+      <div className="ninja">
+        <h2>Name: Ryu</h2>
+        <p>Belt: black</p>
+        <p>Age: 30</p>
+      </div>
+    );
+  }
+}
